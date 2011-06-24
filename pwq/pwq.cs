d36df17ca9c238c1a5c45ef8557df497871ba987
Kraitical <kraitical@gmail.com>
2011-06-24 18:38:18 +0200

    Simplified LatestEpisode, changed method to date-based intstead of return-highest-anime-epnum

--- pwq/pwq.cs
+++ pwq/pwq.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.IO;
 using System.Collections.Generic;
 using System.Web;
+using System.Text.RegularExpressions;
 
 
 namespace pwq.Plugin
@@ -34,84 +35,37 @@ namespace pwq.Plugin
         public int LatestEpisode(int id)
         {
             string anime = listname[Libs.IdOf(ids, id)];
-            WebClient wc = new WebClient();
             string link = "http://www.nyaa.eu/?page=rss&user=63729";
-            byte[] buffer = wc.DownloadData(link);
-            MemoryStream ms = new MemoryStream(buffer);
-            StreamReader sr = new StreamReader(ms);
-            string page = sr.ReadToEnd();
+            byte[] buffer = Libs.DownloadData(link);
+            string page = System.Text.Encoding.UTF8.GetString(buffer);
+            Regex r = new Regex("(?<=(<title>)).*?(?=</title>)", RegexOptions.Singleline);
             List<string> eps = new List<string>();
-            int start = page.IndexOf("<item>");
-            do
+            foreach (Match m in r.Matches(page))
             {
-                start = page.IndexOf("<title>", start) + 7;
-                if (start < 7)
-                {
-                    break;
-                }
-                int end = page.IndexOf("</title>", start);
-                string ep = page.Substring(start, end - start);
-                ep = HttpUtility.HtmlDecode(ep);
-                if (ep.Contains(anime))
-                    eps.Add(ep);
-            } while (true);
+                string line = Libs.UnEscapeHtml(m.Value);
+                if (line.Contains(anime))
+                    eps.Add(line);
+            }
             List<int> epnums = eps.ConvertAll<int>(new Converter<string, int>(AnimeTitleToEpNum));
-            epnums.RemoveAll(new Predicate<int>(IsEp));
-            epnums.Sort(new Comparison<int>(SortComp));
-
+            epnums.RemoveAll(new Predicate<int>(NotEp));
             return epnums[0];
-
         }
-        private bool IsEp(int num)
+        private bool NotEp(int num)
         {
             return num < 0;
         }
-        private int SortComp(int x, int y)
-        {
-            return y - x;
-        }
         private int AnimeTitleToEpNum(string line)
         {
-            //line = HttpUtility.HtmlDecode(line); // shouldn't be needed
-            do
-            {
-                int st = line.IndexOf('[');
-                int end = line.IndexOf(']') + 1;
-                if (st < 0 || end < 0)
-                    break;
-                line = line.Remove(st, end - st);
-            } while (true);
-            int nst = line.LastIndexOf("- ") + 2;
-            //int nend = line.LastIndexOfAny(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }) +1;
-
-            if (nst < 2)
-                return -1;
-            int nend = line.IndexOf(' ', nst);
-            if (nend < 0)
-                return -1;
-            line = line.Substring(nst, nend - nst); // still contains v0s v1s v2s etc
-            nst = line.IndexOf('v');
-            if (nst < 0)
+            string[] ls = line.Split(new char[] { ' ' });
+            foreach (string lin in ls)
             {
                 try
                 {
-                    return Convert.ToInt32(line);
+                    return Convert.ToInt32(lin);
                 }
-                catch
-                {
-                    return -1;
-                }
-
+                catch { }
             }
-            line = line.Remove(nst);
-            try
-            {
-                return Convert.ToInt32(line);
-            }
-            catch
-            {
-                return -1;
-            }
+            return -1;
         }
         public ITlPluginHost Host
         {
